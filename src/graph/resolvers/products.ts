e17fcabf4b276cc26a6d2abdfe49376a38f278de
Yazan Khatib <yazankhatib97@gmail.com>
2020-12-22 16:20:20 +0200

    feat: Search products

--- src/graph/resolvers/products.ts
+++ src/graph/resolvers/products.ts
@@ -49,21 +49,32 @@ export class ProductResolver {
     return favorites;
   }
 
+  @Query(() => [Watch])
+  @UseMiddleware(isAuth)
+  async searchProducts(@Arg('brand') brand: string) {
+    const products = await Watch.query();
+    const result = products.filter((p) => {
+      if (p.brand.search(brand) !== -1) return p;
+    });
+
+    return result;
+  }
+
   @Mutation(() => Watch)
   @UseMiddleware(isAuth)
   async addProduct(
     @Arg('brand') brand: string,
     @Arg('model') model: string,
-    @Arg('description') description: string,
-    @Arg('movement') movement: string,
-    @Arg('case_material') case_material: string,
-    @Arg('bracelet_material') bracelet_material: string,
-    @Arg('production_year') production_year: number,
-    @Arg('condition') condition: string,
     @Arg('delivery') delivery: string,
-    @Arg('gender') gender: string,
+    @Arg('condition') condition: string,
+    @Arg('description') description: string,
     @Arg('location') location: string,
-    @Arg('featured') featured: boolean,
+    @Arg('movement', { nullable: true }) movement: string,
+    @Arg('case_material', { nullable: true }) case_material: string,
+    @Arg('bracelet_material', { nullable: true }) bracelet_material: string,
+    @Arg('production_year', { nullable: true }) production_year: number,
+    @Arg('gender', { nullable: true }) gender: string,
+    @Arg('featured', { nullable: true }) featured: boolean,
 
     // Calibar Optional
     @Arg('calibar', { nullable: true }) calibar: string,
@@ -117,10 +128,129 @@ export class ProductResolver {
 
   @Mutation(() => Watch)
   @UseMiddleware(isAuth)
-  async updateProduct(@Arg('id') id: string) {
-    const product = await Watch.query().findById(id).patch({});
+  async updateProduct(
+    @Arg('id') id: string,
+    @Arg('brand', { nullable: true }) brand: string,
+    @Arg('model', { nullable: true }) model: string,
+    @Arg('description', { nullable: true }) description: string,
+    @Arg('movement', { nullable: true }) movement: string,
+    @Arg('case_material', { nullable: true }) case_material: string,
+    @Arg('bracelet_material', { nullable: true }) bracelet_material: string,
+    @Arg('production_year', { nullable: true }) production_year: number,
+    @Arg('condition', { nullable: true }) condition: string,
+    @Arg('delivery', { nullable: true }) delivery: string,
+    @Arg('gender', { nullable: true }) gender: string,
+    @Arg('location', { nullable: true }) location: string,
+    @Arg('featured', { nullable: true }) featured: boolean,
+
+    // Calibar Optional
+    @Arg('calibar', { nullable: true }) calibar: string,
+    @Arg('base_calibar', { nullable: true }) base_calibar: string,
+    @Arg('power_reserve', { nullable: true }) power_reserve: number,
+    @Arg('jewels', { nullable: true }) jewels: number,
+
+    // Case Optional
+    @Arg('case_diameter', { nullable: true }) case_diameter: number,
+    @Arg('water_resistance', { nullable: true }) water_resistance: number,
+    @Arg('bezel_material', { nullable: true }) bezel_material: string,
+    @Arg('crystal', { nullable: true }) crystal: string,
+    @Arg('dial', { nullable: true }) dial: string,
+    @Arg('dial_numbers', { nullable: true }) dial_numbers: string,
+
+    // Bracelet/strap Optional
+    @Arg('bracelet_color', { nullable: true }) bracelet_color: string,
+    @Arg('clasp', { nullable: true }) clasp: string,
+    @Arg('clasp_material', { nullable: true }) clasp_material: string,
+  ) {
+    const product = await Watch.query().findById(id).patch({
+      brand,
+      model,
+      description,
+      movement,
+      case_material,
+      bracelet_material,
+      production_year,
+      condition,
+      delivery,
+      gender,
+      location,
+      featured,
+      calibar,
+      base_calibar,
+      power_reserve,
+      jewels,
+      case_diameter,
+      water_resistance,
+      bezel_material,
+      crystal,
+      dial,
+      dial_numbers,
+      bracelet_color,
+      clasp,
+      clasp_material,
+    });
 
     return product;
+  }
+
+  @Mutation(() => [Watch])
+  @UseMiddleware(isAuth)
+  async filterProducts(
+    @Arg('brand', { nullable: true }) brand: string,
+    @Arg('model', { nullable: true }) model: string,
+    @Arg('movement', { nullable: true }) movement: string,
+    @Arg('case_material', { nullable: true }) case_material: string,
+    @Arg('bracelet_material', { nullable: true }) bracelet_material: string,
+    @Arg('production_year', { nullable: true }) production_year: number,
+    @Arg('condition', { nullable: true }) condition: string,
+    @Arg('delivery', { nullable: true }) delivery: string,
+    @Arg('gender', { nullable: true }) gender: string,
+    @Arg('location', { nullable: true }) location: string,
+
+    // Calibar Optional
+    @Arg('calibar', { nullable: true }) calibar: string,
+    @Arg('base_calibar', { nullable: true }) base_calibar: string,
+    @Arg('power_reserve', { nullable: true }) power_reserve: number,
+    @Arg('jewels', { nullable: true }) jewels: number,
+
+    // Case Optional
+    @Arg('case_diameter', { nullable: true }) case_diameter: number,
+    @Arg('water_resistance', { nullable: true }) water_resistance: number,
+    @Arg('bezel_material', { nullable: true }) bezel_material: string,
+    @Arg('crystal', { nullable: true }) crystal: string,
+    @Arg('dial', { nullable: true }) dial: string,
+    @Arg('dial_numbers', { nullable: true }) dial_numbers: string,
+
+    // Bracelet/strap Optional
+    @Arg('bracelet_color', { nullable: true }) bracelet_color: string,
+    @Arg('clasp', { nullable: true }) clasp: string,
+    @Arg('clasp_material', { nullable: true }) clasp_material: string,
+  ) {
+    const watches = Watch.query()
+      .where('brand', brand)
+      .where('model', model)
+      .where('movement', movement)
+      .where('case_material', case_material)
+      .where('bracelet_material', bracelet_material)
+      .where('production_year', production_year)
+      .where('condition', condition)
+      .where('delivery', delivery)
+      .where('gender', gender)
+      .where('location', location)
+      .where('calibar', calibar)
+      .where('base_calibar', base_calibar)
+      .where('power_reserve', power_reserve)
+      .where('case_diameter', case_diameter)
+      .where('water_resistance', water_resistance)
+      .where('bezel_material', bezel_material)
+      .where('crystal', crystal)
+      .where('dial', dial)
+      .where('dial_numbers', dial_numbers)
+      .where('bracelet_color', bracelet_color)
+      .where('clasp', clasp)
+      .where('clasp_material', clasp_material);
+
+    return watches;
   }
 
   @Mutation(() => Watch)
