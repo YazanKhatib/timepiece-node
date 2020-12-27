d4606e860e01f4df109b509e304c136216009b95
Yazan Khatib <yazankhatib97@gmail.com>
2020-12-27 15:13:44 +0200

    feat: username or email login

--- src/graph/resolvers/auth.ts
+++ src/graph/resolvers/auth.ts
@@ -66,13 +66,16 @@ export class AuthResolver {
 
   @Mutation(() => LoginResponse)
   async login(
-    @Arg('email') email: string,
-    @Arg('username', { nullable: true, defaultValue: undefined })
-    username: string,
+    @Arg('email', { defaultValue: null }) email: string,
+    @Arg('username', { defaultValue: null }) username: string,
     @Arg('password') password: string,
   ): Promise<LoginResponse> {
     let user;
-    if (username === undefined) {
+    if (email === null && username === null) {
+      throw new Error('Username or email must be provided!');
+    }
+
+    if (username === null) {
       user = await User.query().findOne('email', email);
     } else user = await User.query().findOne('username', username);
 
