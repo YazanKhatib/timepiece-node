846f9d11251fe7c547ff678558dbcec0729e6d0b
Yazan Khatib <yazankhatib97@gmail.com>
2021-01-20 17:01:41 +0200

    fix: heroku port

--- src/graph/index.ts
+++ src/graph/index.ts
@@ -42,7 +42,7 @@ export const startServer = async (app: Application) => {
 
   server.applyMiddleware({ app });
 
-  app.listen(80, () =>
+  app.listen(process.env.PORT || 80, () =>
     Logger.info(
       `🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`,
     ),
