6911095970ea1c06ea601fe48c30cbfed736ee44
y.khatib <y.khatib@zkood.com>
2021-09-20 16:39:00 +0300

    fix: account confirmation

--- src/graph/resolvers/auth.ts
+++ src/graph/resolvers/auth.ts
@@ -59,7 +59,7 @@ export class AuthResolver {
       throw new Error('Could not find user!');
     }
 
-    if (!user.confirmed || user.blocked) {
+    if (user.blocked) {
       throw new Error('User account is not confirmed!');
     }
 
